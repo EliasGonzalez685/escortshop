7ef0589c25f3e07bbb0dbaef1178ee2af6c2683d
Elias Gonzalez <eliasgonzalez685@gmail.com>
2026-01-07 02:05:54 -0300

    Agregar 'use client' a componentes con hooks de React

--- app/[categoria]/page.jsx
+++ app/[categoria]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../lib/supabase'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useParams, useRouter } from 'next/navigation'
